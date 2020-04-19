3f3d0bc6d522c7ef6ae90225ef3d0a212997af93
Max Van Aken <maxvanaken@Maxs-MBP.home.ukb>
2020-04-19 02:34:36 +0100

    Filter links

--- server/components/searchController.js
+++ server/components/searchController.js
@@ -1,3 +1,10 @@
 const scrape = require('./utils/scraper')
 
-scrape()
+const controller = async (subreddit, filter) => {
+    const rawLinks = await scrape(subreddit, filter)
+    const filtered = []
+    rawLinks.map((link) => {
+        (link.includes("reddit") || link.includes(subreddit) || link.includes("/user/")) ? null : filtered.push(link)
+    })
+    return filtered
+}
--- server/components/utils/scraper.js
+++ server/components/utils/scraper.js
@@ -2,17 +2,19 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const scrape = async (subreddit, filter) => {
-  
-
-
-    const html = await axios.get('https://www.reddit.com/r/TrueHouse/')
+  const subredditLink = "https://www.reddit.com/r/" + subreddit + "/top/?t=" + filter
+  const html = await axios.get(subredditLink)
+  try {
     const $ = cheerio.load(html.data)
     const links = $('a')
     const data = []
-    $(links).each(function(i, link){
-        data.push(link.attribs.href)
-      });
-      return data
+    $(links).each((i,link) => {
+      data.push(link.attribs.href)
+    });
+    return data
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 module.exports = scrape
